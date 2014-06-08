e9bb718f0c7a018ca0ccb635164eacc4a3b8457e
zenparsing <zenparsing@gmail.com>
2014-06-07 23:44:47 -0400

    Make color functions actually return a string.

--- src/ConsoleStyle.js
+++ src/ConsoleStyle.js
@@ -4,8 +4,8 @@ export var ConsoleStyle = {
 
     red(msg) { return `\x1B[31m${ msg }\x1B[39m` },
 
-    gray(msg) { `\x1B[90m${ msg }\x1B[39m` },
+    gray(msg) { return `\x1B[90m${ msg }\x1B[39m` },
 
-    bold(msg) { `\x1B[1m${ msg }\x1B[22m` },
+    bold(msg) { return `\x1B[1m${ msg }\x1B[22m` },
 
 };
